fa4868ecd1350822542b17e1edb4265551e63b38
Muhammad Abdullah Tahir <aby217@gmail.com>
2017-09-11 23:09:07 +0500

    miwok app

---
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,52 @@
+package com.example.android.miwok;
+
+/**
+ * Created by Abdullah Tahir on 7/17/2017.
+ */
+
+public class Word {
+
+
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
+    private int mAudioResourceId;
+
+
+    public Word(String mDefaultTranslation, String mMiwokTranslation, int mAudioResourceId) {
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mMiwokTranslation = mMiwokTranslation;
+        this.mAudioResourceId = mAudioResourceId;
+
+
+    }
+
+    public Word(String mDefaultTranslation, String mMiwokTranslation, int mImageResourceId, int mAudioResourceId) {
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mMiwokTranslation = mMiwokTranslation;
+        this.mImageResourceId = mImageResourceId;
+        this.mAudioResourceId = mAudioResourceId;
+
+    }
+
+    public String getmDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getmMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public int getmImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+}
